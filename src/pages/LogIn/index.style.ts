04a0a5652e6093652aab233910fe6545d9a50c0e
Ramon K. Shin <gml4009@gmail.com>
2022-12-16 20:17:57 +0900

    Img added & Login Page Style Complete

--- src/pages/LogIn/index.style.ts
+++ src/pages/LogIn/index.style.ts
@@ -5,6 +5,9 @@ export const PageWrapper = styled.section`
   height: 100%;
   display: flex;
   justify-content: center;
+  --main-bg-color: #f5ac2d;
+  --stby-bg-color: #c79114;
+  --dark-bg-color: #744308;
 
   div.page-body {
     display: flex;
@@ -17,8 +20,24 @@ export const PageWrapper = styled.section`
       margin-top: 24px;
     }
 
+    input:focus {
+      outline: 2px solid var(--main-bg-color);
+    }
+
     button {
       margin-top: 40px;
+      background-color: var(--main-bg-color);
+      transition: 0.2s ease;
+      border: 0;
     }
+    
+    button:hover {
+      background-color: var(--stby-bg-color);
+    }
+    
+    button:active {
+      background-color: var(--dark-bg-color);
+    }
+
   }
 `;
